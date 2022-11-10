0b0397c63104db2e87c6f455bf77fa06a0ddbb33
Ryan Menezes <menezesryan1010@gmail.com>
2022-11-10 20:31:23 -0300

    Creating the Sql\Dql\Where class

---
+++ src/Sql/Dql/Where.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QueryBuilder\Sql\Dql;
+
+use QueryBuilder\Interfaces\SqlInterface;
+use QueryBuilder\Sql\{
+    Column,
+    Field,
+};
+
+class Where implements SqlInterface
+{
+    private array $logicalInstructions = [];
+
+    public function and(string|Column $column, string $operator, mixed $value): self
+    {
+        $field = new Field($column, $operator, $value);
+
+        $this->addLogicalInstruction('AND', $field);
+
+        return $this;
+    }
+
+    public function or(string|Column $column, string $operator, mixed $value): self
+    {
+        $field = new Field($column, $operator, $value);
+
+        $this->addLogicalInstruction('OR', $field);
+
+        return $this;
+    }
+
+    private function addLogicalInstruction(string $conditional, Field $field): void
+    {
+        if($this->isEmptyLogicalInstructionsArray()) {
+            $this->logicalInstructions[] = $field;
+            return;
+        }
+
+        $this->logicalInstructions[] = $conditional;
+        $this->logicalInstructions[] = $field;
+    }
+
+    private function isEmptyLogicalInstructionsArray(): bool
+    {
+        return empty($this->getLogicalInstructions());
+    }
+
+    public function __toString(): string
+    {
+        $sqlFields = implode(' ', $this->getLogicalInstructions());
+        return "WHERE {$sqlFields}";
+    }
+
+    public function getLogicalInstructions(): array
+    {
+        return $this->logicalInstructions;
+    }
+}
---
+++ tests/Sql/Dql/WhereTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Sql;
+
+use PHPUnit\Framework\TestCase;
+
+use QueryBuilder\Sql\Dql\Where;
+use QueryBuilder\Sql\Field;
+use QueryBuilder\Sql\Values\RawValue;
+
+/**
+ * @requires PHP 8.1
+ */
+class WhereTest extends TestCase
+{
+    public function testShouldCreateWHEREStatementsWithLogicalAndComparisonOperators()
+    {
+        $where = new Where();
+        $where
+            ->and('name', 'LIKE', '%any-name%')
+            ->or('salary', '>', 800)
+            ->and('isTeacher', '=', true)
+            ->or('age', 'BETWEEN', new RawValue('20 AND 30'))
+            ->or('created_at', '=', null);
+
+        $this->assertEquals('WHERE `name` LIKE ? OR `salary` > ? AND `isTeacher` = ? OR `age` BETWEEN 20 AND 30 OR `created_at` = ?', $where);
+        $this->assertEquals([
+            new Field('name', 'LIKE', '%any-name%'),
+            'OR',
+            new Field('salary', '>', 800),
+            'AND',
+            new Field('isTeacher', '=', true),
+            'OR',
+            new Field('age', 'BETWEEN', new RawValue('20 AND 30')),
+            'OR',
+            new Field('created_at', '=', null),
+        ], $where->getLogicalInstructions());
+    }
+}
