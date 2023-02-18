7d2b64a716801e42641bf70623e71012594f073b
Ryan Menezes <menezesryan1010@gmail.com>
2023-02-18 16:49:53 -0300

    Creating the class that will generate the OFFSET command

---
+++ tests/Sql/Operators/OffsetTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Sql\Operators\Logical;
+
+use PHPUnit\Framework\TestCase;
+use QueryBuilder\Sql\Values\StringValue;
+use QueryBuilder\Sql\Operators\Offset;
+use QueryBuilder\Interfaces\SqlWithValuesInterface;
+
+/**
+ * @requires PHP 8.1
+ */
+class OffsetTest extends TestCase
+{
+    public function makeSut(int $value): Offset
+    {
+        $sqlMock = $this->createMock(SqlWithValuesInterface::class);
+        $sqlMock
+            ->method('__toString')
+            ->willReturn('SELECT CONTACT(name, ?) FROM `any-table` LIMIT 10');
+        $sqlMock
+            ->method('toSql')
+            ->willReturn('SELECT CONTACT(name, ?) FROM `any-table` LIMIT 10');
+        $sqlMock
+            ->method('getValues')
+            ->willReturn([new StringValue('any-value-sql')]);
+
+        return new Offset($sqlMock, $value);
+    }
+
+    public function testShouldCreateAnOffsetStatementCorrectly()
+    {
+        $sut = $this->makeSut(0);
+
+        $this->assertEquals(
+            'SELECT CONTACT(name, ?) FROM `any-table` LIMIT 10 OFFSET 0',
+            $sut->toSql(),
+        );
+    }
+}
